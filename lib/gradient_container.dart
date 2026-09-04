3816fb3d94677c028fd6357d11aad1d49fedd554
ANWESHA DAS <anweshadas1302@gmail.com>
2026-09-05 00:46:48 +0530

    dice roll

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_application_1/diceRoller.dart';
+
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  GradientContainer(this.color1, this.color2, {super.key});
-  GradientContainer.purple({super.key})
-    : color1 = const Color.fromARGB(255, 246, 154, 126),
-      color2 = const Color.fromARGB(255, 243, 199, 103);
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+    : color1 = Colors.deepPurple,
+      color2 = Colors.indigo;
+
   final Color color1;
   final Color color2;
-
-  var activeToyimage = 'assets/images/a1.png';
-  void navigateToSecondScreen() {
-    activeToyimage = 'assets/images/a2.png';
-    print('Changing image...');
-  }
 
   @override
   Widget build(context) {
@@ -27,28 +25,29 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(activeToyimage, width: 200),
-            TextButton(
-              onPressed: () {
-                navigateToSecondScreen();
-              },
-              style: TextButton.styleFrom(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 24,
-                  vertical: 12,
-                ),
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 28),
-              ),
-              child: const Text('Go to Second Screen'),
-            ),
-          ],
-        ),
-      ),
+      child: Center(child: DiceRoller()),
     );
   }
 }
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+
+//   final List<Color> colors;
+
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyledText('Hello World!'),
+//       ),
+//     );
+//   }
+// }
